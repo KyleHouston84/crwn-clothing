7b67cbe9520e427933ea4cd56bbb730ac31b2379
kyle <kyle@melissadata.com>
2022-02-15 23:48:11 -0800

    Initial commit for the sign in/up components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import Header from './components/header/header.component';
 import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={ <HomePage /> }/>
-        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/shop' element={ <ShopPage /> } />
+        <Route path='/signin' element={ <SignInAndSignUp /> } />
       </Routes>
     </div>
   );
