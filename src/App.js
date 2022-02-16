eb960aff9694525c7ddec81f33f58520e7ed1b16
kyle <kyle@melissadata.com>
2022-02-15 21:08:58 -0800

    Added react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={ <HomePage /> }/>
+        <Route path='/hats' element={<Hats />} />
+      </Routes>
     </div>
   );
+}
+
+function Hats() {
+  return <div> <h1>HATS</h1> </div>
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+  <Link className={`${size ? size : ''} menu-item`} to={linkUrl}>
     <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} ></div>
     <div className="content">
       <h1 className="title">{ title }</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
-  </div>
+  </Link>
 );
 
 export default MenuItem;
