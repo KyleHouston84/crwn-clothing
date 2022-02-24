fb974c9ea389e835b9c0d3291cbbf150d1c5079b
kyle <kyle@melissadata.com>
2022-02-23 23:01:59 -0800

    Used as='' instead of seperate Div and Link styles in the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selector";
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import { HeaderConatiner, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
+import { HeaderConatiner, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
@@ -23,7 +23,7 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to='/shop'>CONTACT</OptionLink>
       {
         currentUser 
-          ? <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+          ? <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
           : <OptionLink to="/signin">SIGN IN</OptionLink> 
       }
       <CartIcon />  
